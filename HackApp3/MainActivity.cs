572640e18636c36582503c4eaa7b9f1742a75d1f
Lukas Narusevicius <luna@esri-sgroup.com>
2017-11-28 16:22:35 +0200

    location

--- HackApp3/MainActivity.cs
+++ HackApp3/MainActivity.cs
@@ -4,50 +4,80 @@ using Android.Content.PM;
 using Android.OS;
 using Esri.ArcGISRuntime.UI.Controls;
 using Android.Widget;
+using System;
+using Esri.ArcGISRuntime.UI;
 
 namespace HackApp3
 {
-	[Activity (Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : Activity
-    {
-        MapViewModel _mapViewModel = new MapViewModel();
-        MapView _mapView;
+	[Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : Activity
+	{
+		MapViewModel _mapViewModel = new MapViewModel();
+		MapView _mapView;
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+		private ViewState m_CurrentView = ViewState.Main;
 
-            // Set the view from the "Main" layout resource
-            SetContentView(Resource.Layout.Main);
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set the view from the "Main" layout resource
+			SetContentView(Resource.Layout.Main);
 
 			var btn_Register = FindViewById<Button>(Resource.Id.RegisterButton);
 			var btn_Anon = FindViewById<Button>(Resource.Id.AnonymousButton);
 
-			btn_Register.Click += (s, arg) =>
+			btn_Register.Click += RegisterClicked;
+			btn_Anon.Click += AnonClicked;
+		}
+
+		public override void OnBackPressed()
+		{
+			switch (m_CurrentView)
 			{
-				SetContentView(Resource.Layout.Register);
-			};
+				case ViewState.Map:
+					SetContentView(Resource.Layout.Main);
+					break;
+				case ViewState.Register:
+					SetContentView(Resource.Layout.Main);
+					break;
+				default:
+					break;
+			}
+		}
 
-			btn_Anon.Click += (s, arg) =>
-			{
-				SetContentView(Resource.Layout.Map);
-				//RenderMap();
+		private void RegisterClicked(object sender, EventArgs ea)
+		{
+			SetContentView(Resource.Layout.Register);
+			m_CurrentView = ViewState.Register;
+		}
 
-				////Get MapView from the view and assign map from view-model
-				//_mapView = FindViewById<MapView>(Resource.Id.MyMapView);
-				//_mapView.Map = _mapViewModel.Map;
+		private void AnonClicked(object sender, EventArgs ea)
+		{
+			SetContentView(Resource.Layout.Map);
+			m_CurrentView = ViewState.Register;
+			RenderMap();
+		}
 
-				//// Listen for changes on the view model
-				//_mapViewModel.PropertyChanged += MapViewModel_PropertyChanged;
-			};
+		private void RenderMap()
+		{
+			//Get MapView from the view and assign map from view-model
+			_mapView = FindViewById<MapView>(Resource.Id.MyMapView);
+			_mapView.Map = _mapViewModel.Map;
+
+			// Listen for changes on the view model
+			_mapViewModel.PropertyChanged += MapViewModel_PropertyChanged;
+
+			_mapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
+			_mapView.LocationDisplay.IsEnabled = true;
 
 		}
 
-        private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            // Update the map view with the view model's new map
-            if (e.PropertyName == "Map" && _mapView != null)
-                _mapView.Map = _mapViewModel.Map;
-        }
-    }
+		private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			// Update the map view with the view model's new map
+			if (e.PropertyName == "Map" && _mapView != null)
+				_mapView.Map = _mapViewModel.Map;
+		}
+	}
 }
