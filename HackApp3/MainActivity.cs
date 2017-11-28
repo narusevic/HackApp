ffc506115cf627389875ea0745a331736d3fa8b6
Lukas Narusevicius <luna@esri-sgroup.com>
2017-11-28 18:05:22 +0200

    map marker

--- HackApp3/MainActivity.cs
+++ HackApp3/MainActivity.cs
@@ -6,16 +6,17 @@ using Android.Widget;
 using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using Esri.ArcGISRuntime.UI;
+using System.Drawing;
+using Esri.ArcGISRuntime.Symbology;
 
 namespace HackApp3
 {
 	[Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : Activity
 	{
-		MapViewModel _mapViewModel = new MapViewModel();
-		MapView _mapView;
-
-		private ViewState m_CurrentView = ViewState.Main;
+		private MapViewModel _mapViewModel = new MapViewModel();
+		private MapView _mapView;
+		private GraphicsOverlay _sketchOverlay;
 
 		protected override void OnCreate(Bundle bundle)
 		{
@@ -60,13 +61,52 @@ namespace HackApp3
 
 			//Get MapView from the view and assign map from view-model
 			_mapView = FindViewById<MapView>(Resource.Id.MyMapView);
+			var btn_Mark = FindViewById<Button>(Resource.Id.Mark);
+
+			btn_Mark.Click += MarkOnMap;
+
+			_sketchOverlay = new GraphicsOverlay();
+			_mapView.GraphicsOverlays.Add(_sketchOverlay);
+
+			var markerSymbol = new SimpleMarkerSymbol()
+			{
+				Color = Color.Red,
+				Style = SimpleMarkerSymbolStyle.Cross,
+				Size = 30d
+			};
+
+			foreach (var geometry in Geometries.GetAll())
+			{
+				_sketchOverlay.Graphics.Add(new Graphic(geometry, markerSymbol));
+			}
+
 			_mapView.Map = _mapViewModel.Map;
 
 			// Listen for changes on the view model
 			_mapViewModel.PropertyChanged += MapViewModel_PropertyChanged;
-			_mapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
+			//_mapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
 			_mapView.LocationDisplay.IsEnabled = true;
+		}
 
+		private void MarkOnMap(object sender, System.EventArgs e)
+		{
+			var btn_Mark = FindViewById<Button>(Resource.Id.Mark);
+			//btn_Mark.Enabled = false;
+
+			SketchCreationMode creationMode = SketchCreationMode.Point;
+			//Esri.ArcGISRuntime.Geometry.Geometry geometry = await _mapView.SketchEditor.StartAsync(creationMode, true);
+			
+			//btn_Mark.Enabled = true;
+			btn_Mark.Click += MarkOnMap;
+
+			Graphic graphic = new Graphic(_mapView.LocationDisplay.MapLocation, new SimpleMarkerSymbol()
+			{
+				Color = Color.Red,
+				Style = SimpleMarkerSymbolStyle.Cross,
+				Size = 30d
+			});
+
+			_sketchOverlay.Graphics.Add(graphic);
 		}
 
 		private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
--- HackApp3/Resources/Resource.Designer.cs
+++ HackApp3/Resources/Resource.Designer.cs
@@ -67,10 +67,13 @@ namespace HackApp3
 			public const int AnonymousButton = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int MyMapView = 2131034114;
+			public const int Mark = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int RegisterBackButton = 2131034115;
+			public const int MyMapView = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int RegisterBackButton = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int RegisterButton = 2131034112;
